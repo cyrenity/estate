83f855fa9b1eded742ec00d9e7cc32f86cb9c7fc
Ghulam Mustafa <mustafa.pk@gmail.com>
2022-03-08 16:18:59 +0000

    -Fixed Many2one relation between offers and properties table

--- models/estate_property.py
+++ models/estate_property.py
@@ -38,4 +38,6 @@ class EstateProperty(models.Model):
     
     tag_ids = fields.Many2many("estate.property.tag", string="Tags")
     
-    offer_ids = fields.One2many("estate.property.offer", "partner_id", string="Offers")
+    offer_ids = fields.One2many("estate.property.offer", "property_id", string="Offers")
+    
+    
