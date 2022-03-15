68190f15e96e0209ca5bd9d5efed3aacee8ca8e4
Ghulam Mustafa <mustafa.pk@gmail.com>
2022-03-15 09:09:44 +0000

    -Added properties view in user view

--- models/res_users.py
+++ models/res_users.py
@@ -7,4 +7,5 @@ from odoo import fields, models
 class Users(models.Model):    
     _inherit = 'res.users'
     
-    property_ids = fields.One2many('estate.property', inverse_name='salesperson', domain="[('state', 'in', ('offer_received', 'new'))]")
+    property_ids = fields.One2many('estate.property', inverse_name='salesperson', domain=lambda self: [('state', 'in', ('new', 'offer_received'))])
+    
